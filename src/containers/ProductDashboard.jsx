58e4b1238a2f3117775b636f91cf97a72dbeb096
Arun Ramachandran <arun.ramachandran6@wipro.com>
2020-01-27 19:19:49 +0000

    Added individual product dashbaord

--- src/containers/ProductDashboard.jsx
+++ src/containers/ProductDashboard.jsx
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
-import store from '../store/configStore';
 import { connect } from 'react-redux';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 
 class ProductDashboard extends Component {
 
     render () {
         const data = this.props.selectedProduct;
         console.log("User selected data: ", data);
+        const classes = makeStyles(theme => ({
+            root: {
+              flexGrow: 1,
+            },
+            paper: {
+              padding: theme.spacing(2),
+              margin: 'auto',
+              maxWidth: 500,
+            },
+            image: {
+              width: 128,
+              height: 128,
+            },
+            img: {
+              margin: 'auto',
+              display: 'block',
+              maxWidth: '100%',
+              maxHeight: '100%',
+            },
+          }));
 
         return (
-            <div className="product-dashboard">
-                <h1>Product Dashboard</h1>
+            <div className={classes.root}>
+                <Paper className={classes.paper}>
+                    <Grid container spacing={2}>
+                    <Grid item>
+                        <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src={data.image_url} />
+                        </ButtonBase>
+                    </Grid>
+                    <Grid item xs={12} sm container>
+                        <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                            <Typography gutterBottom variant="subtitle1">
+                                { data.name }
+                            </Typography>
+                            <Typography variant="body2" gutterBottom>
+                                { data.tagline }
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                { data.abv }
+                            </Typography>
+                            <Typography variant="body2" gutterBottom>
+                                { data.description }
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                                Remove from favourites
+                            </Typography>
+                        </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="subtitle1">first brewed : {data.first_brewed}</Typography>
+                        </Grid>
+                    </Grid>
+                    </Grid>
+                </Paper>
             </div>
         )
     }
